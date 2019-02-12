a7fabb3dfe3f46e9e891368f51c873278e054f4c
Nurda <m.n.m.nurdaulet@gmail.com>
2019-02-12 12:13:01 -0800

    Setting UP

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,19 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <h1>About us</h1>
   </div>
 </template>
 
-<script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
 
+<script>
 export default {
-  name: 'home',
-  components: {
-    HelloWorld
-  }
+ 
 }
 </script>
+
+
+<style scoped> 
+	.home{
+		padding:15px;
+	}
+</style>
